95a85bbd06a2d17071fc035be8f7e372e254847c
sleepsim <91400544+sleepsim@users.noreply.github.com>
2022-03-10 01:37:12 -0800

    Working Tabs

--- app/src/main/java/com/example/a359personalproject/ListCategory.kt
+++ app/src/main/java/com/example/a359personalproject/ListCategory.kt
@@ -1,9 +1,9 @@
 package com.example.a359personalproject
 
-class category (categoryName: String){
+class ListCategory (categoryName: String){
 
     private var items:ArrayList<String> = ArrayList<String>()
-    val name = categoryName
+    val name: String = categoryName
 
     public fun addItem(item:String){
         val text = item
@@ -14,6 +14,10 @@ class category (categoryName: String){
         val text = item
         val indexNum = items.indexOf(text)
         items.removeAt(indexNum)
+    }
+
+    public fun getCategoryName(): String{
+        return name
     }
 
 }
--- app/src/main/java/com/example/a359personalproject/MainActivity.kt
+++ app/src/main/java/com/example/a359personalproject/MainActivity.kt
@@ -1,27 +1,61 @@
 package com.example.a359personalproject
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.tabs.TabLayout
-import androidx.viewpager.widget.ViewPager
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-import com.google.android.material.tabs.TabItem
 
 class MainActivity : AppCompatActivity() {
+
+    var categories:ArrayList<ListCategory> = ArrayList<ListCategory>()
+    var categoryNames:ArrayList<String> = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val mainTabLayout = findViewById<TabLayout>(R.id.categoriesTab)
-        mainTabLayout.contentDescription = "Different Categories for Lists"
+        categories.add(ListCategory("Main"))
+        categories.add(ListCategory("new"))
 
-        val tab1:TabLayout.Tab = mainTabLayout.newTab()
-        tab1.text = "test lol"
+        if(savedInstanceState == null) {
+            //initilize
+            initiliazeApp()
+            //Initial fragment loaded first open
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragmentContainer,
+                    Fragment_main.newInstance(),
+                    "MyTag"
+                ).commit()
+        }
+    }
 
-        mainTabLayout.addTab(tab1)
+    private fun initiliazeApp() {
+        //Set up categories and drop down menu
+        for (i in categories){
+            categoryNames.add(i.getCategoryName())
+        }
+
+        val arrayAdapter = ArrayAdapter(this@MainActivity, android.R.layout.simple_spinner_dropdown_item, categoryNames)
+        val spinner = findViewById<Spinner>(R.id.spinner)
+        spinner.adapter = arrayAdapter
+        spinner.onItemSelectedListener = object :AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+
+        }
+
+//        val spinner = findViewById<Spinner>(R.id.spinner)
+//        categories.add(ListCategory("Main"))
+//        val adapter = ArrayAdapter<ListCategory>(this, android.R.layout.simple_spinner_dropdown_item, categories)
+//        spinner.adapter(adapter)
+
     }
 }
