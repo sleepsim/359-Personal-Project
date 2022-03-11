6973682c55c5d862bbb7df68a9d5d4bd17fde513
sleepsim <91400544+sleepsim@users.noreply.github.com>
2022-03-10 22:36:04 -0800

    Added add function to tabs

--- app/src/main/java/com/example/a359personalproject/MainActivity.kt
+++ app/src/main/java/com/example/a359personalproject/MainActivity.kt
@@ -1,16 +1,18 @@
 package com.example.a359personalproject
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
-    var categories:ArrayList<ListCategory> = ArrayList<ListCategory>()
-    var categoryNames:ArrayList<String> = ArrayList<String>()
+    private var categories:ArrayList<ListCategory> = ArrayList<ListCategory>()
+    private var categoryNames:ArrayList<String> = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,8 +22,8 @@ class MainActivity : AppCompatActivity() {
         categories.add(ListCategory("new"))
 
         if(savedInstanceState == null) {
-            //initilize
-            initiliazeApp()
+            //initialize
+            initializeApp()
             //Initial fragment loaded first open
             supportFragmentManager
                 .beginTransaction()
@@ -30,16 +32,28 @@ class MainActivity : AppCompatActivity() {
                     "MyTag"
                 ).commit()
         }
+
+        val addNewButton = findViewById<FloatingActionButton>(R.id.addButton)
+        addNewButton.setOnClickListener {
+            categories.add(ListCategory("newly added"))
+            clearAdapter()
+            initializeApp()
+            for (f in categoryNames){
+                Log.i("CATNAMES", f.toString())
+            }
+            Log.i("endOfArr", "END")
+        }
     }
 
-    private fun initiliazeApp() {
+    //Resets the categories
+    private fun initializeApp() {
         //Set up categories and drop down menu
         for (i in categories){
             categoryNames.add(i.getCategoryName())
         }
 
-        val arrayAdapter = ArrayAdapter(this@MainActivity, android.R.layout.simple_spinner_dropdown_item, categoryNames)
         val spinner = findViewById<Spinner>(R.id.spinner)
+        val arrayAdapter = ArrayAdapter(this@MainActivity, android.R.layout.simple_spinner_dropdown_item, categoryNames)
         spinner.adapter = arrayAdapter
         spinner.onItemSelectedListener = object :AdapterView.OnItemSelectedListener{
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
@@ -51,11 +65,20 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
+    }
+    private fun clearAdapter(){
+        val spinner = findViewById<Spinner>(R.id.spinner)
+        val arrayAdapter = ArrayAdapter(this@MainActivity, android.R.layout.simple_spinner_dropdown_item, categoryNames)
+        spinner.adapter = arrayAdapter
+        arrayAdapter.clear()
+        arrayAdapter.notifyDataSetChanged()
+    }
 
-//        val spinner = findViewById<Spinner>(R.id.spinner)
-//        categories.add(ListCategory("Main"))
-//        val adapter = ArrayAdapter<ListCategory>(this, android.R.layout.simple_spinner_dropdown_item, categories)
-//        spinner.adapter(adapter)
+    private fun saveApp(){
+
+    }
+
+    fun addNew(){
 
     }
 }
